1bdaf136223889629744c499a987aa3bfb193ba4
Lívia Macedo <125596597+1iviaMacedo@users.noreply.github.com>
2024-02-20 09:43:37 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { View } from 'react-native';
 import Cabeçalho from './Cabeçalho';
 import Corpo from './Corpo';
 import Rodapé from './Rodapé';
+
+//cada parte, devera conter seu import, de modo a chamar o conteudo da pasta para que apareça em uma tela em específico
+
 const App = () => {
 return (
 <View>
